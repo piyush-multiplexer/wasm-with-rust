7cea8197849db0dea32dbf59bf17537c3a829ec5
Piyush <thesourcepedia@gmail.com>
2022-08-29 13:43:09 +0530

    chore: change package name and add build dir

---
+++ pkg/README.md
@@ -0,0 +1,14 @@
+# wasm-with-rust
+WebAssembly (WASM) with Rust
+
+### Prerequisites:
+- Node and Rust
+
+### Setup in Local Machine:
+```
+git clone https://github.com/piyush-multiplexer/wasm-with-rust.git
+cd wasm-with-rust
+cargo install wasm-pack
+wasm-pack build --target web
+serve .
+```
---
+++ pkg/wasm_with_rust.d.ts
@@ -0,0 +1,35 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} a
+* @param {number} b
+* @param {string} op
+* @returns {number}
+*/
+export function calc(a: number, b: number, op: string): number;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly calc: (a: number, b: number, c: number) => number;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
