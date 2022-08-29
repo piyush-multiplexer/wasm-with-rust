0095ca6c67aae1fcebe98958617cd40048618e7a
Piyush <thesourcepedia@gmail.com>
2022-08-29 13:22:41 +0530

    fix: format with Rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn calc(a: i32, b: i32, op:char)  -> i32 {
+pub fn calc(a: i32, b: i32, op: char) -> i32 {
     if op == '+' {
         return a + b;
-    } else if op == '-'{
+    } else if op == '-' {
         return a - b;
-    }  else if  op == '*'{
+    } else if op == '*' {
         return a * b;
-    }  else if  op == '/'{
+    } else if op == '/' {
         return a / b;
     } else {
         return 0;
-    }  
+    }
 }
