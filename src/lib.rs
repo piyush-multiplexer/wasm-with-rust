3627ee475aa3857bdd382437cd0a5824447c1858
Piyush <thesourcepedia@gmail.com>
2022-08-29 12:55:07 +0530

    feat(wasm): add math func in rust wasm

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn calc(a: i32, b: i32, op:char)  -> i32 {
+    if op == '+' {
+        return a + b;
+    } else if op == '-'{
+        return a - b;
+    }  else if  op == '*'{
+        return a * b;
+    }  else if  op == '/'{
+        return a / b;
+    } else {
+        return 0;
+    }  
+}
